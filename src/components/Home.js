809a16a5de4161b5da9ceed303cc0e5a7c79818b
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-12-02 01:58:50 +0500

    npm i redux-devtools-extension

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Header from "./Header"
-import { useSelector } from "react-redux";
+import {useSelector} from "react-redux";
 import currencyFormatter from "currency-formatter";
-import { Link } from "react-router-dom"
+import {Link} from "react-router-dom"
 const Home = () => {
-    const { products } = useSelector(state => state.ProductsReducer);
+    const {products} = useSelector(state => state.ProductsReducer);
     return (
         <div>
             <Header />
@@ -14,7 +14,7 @@ const Home = () => {
                         <div className="col-3" key={product.id}>
                             <div className="product">
                                 <div className="product__img">
-                                    <Link to={`/details/${product.id}`}><img src={`/images/${product.image}`} alt="image name" /></Link>
+                                    <Link to={`/details/${product.id}`}><img src={`/images/${product.image}`} alt="image name"/></Link>
                                 </div>
                                 <div className="product__name">
                                     {product.name}
@@ -22,12 +22,12 @@ const Home = () => {
                                 <div className="row">
                                     <div className="col-6">
                                         <div className="product__price">
-                                            <span className="actualPrice">{currencyFormatter.format(product.price, { code: 'USD' })}</span> <span className="discount">{product.discount}%</span>
+                    <span className="actualPrice">{currencyFormatter.format(product.price, { code: 'USD' })}</span> <span className="discount">{product.discount}%</span>
                                         </div>
                                     </div>
                                     <div className="col-6">
                                         <div className="product__discount__price">
-                                            {currencyFormatter.format(product.discountPrice, { code: 'USD' })}
+                                        {currencyFormatter.format(product.discountPrice, { code: 'USD' })}
                                         </div>
                                     </div>
                                 </div>
